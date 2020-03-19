02d16bbd32caf8eb3e2303fcbd59e7c1dd0c401c
David Enoma <davidenoma@gmail.com>
2020-03-20 00:02:44 +0100

    ford

--- graphs/dijkstranew.py
+++ graphs/dijkstranew.py
@@ -40,9 +40,37 @@ addEdge(4,3,2)
 addEdge(4,6,8)
 addEdge(4,7,4)
 addEdge(5, 7, 6)
+addEdge(7,6,1)
 
+dictknown = {}
 
+i = 1
+for v in vertices:
+    dictknown[v] = False
+    i+=1   
+distance = {}
+for v in range(1,len(vertices)+1):
+    distance[v] = 0
+    i+=1  
+pv = {}
+for v in vertices:
+    pv[v] = None
+    i+=1  
 print(matrix)
 
+print(pv)
+current = ""
 
+def traverse(initial,target):
+    current = initial
+    dictknown[vtxids[initial]] = True
+    i = 1
+    for x in matrix[initial]:    
+        distance[i] = x
+        if x != 0:
+            pv[i] = vtxids[current]
+        i = i+1
+    
+    
+    
 
